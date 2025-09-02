1c3aeb3205f40be13d6bd9476474ca97a9c629ec
Constantin Heinzler <me@cheinzler.de>
2025-09-02 18:30:19 +0200

    Add cache timestamp and processability

--- PSAzureLocator/Private/New-AzServiceTagCache.ps1
+++ PSAzureLocator/Private/New-AzServiceTagCache.ps1
@@ -7,16 +7,19 @@ Function New-AzServiceTagCache {
 
     If ($Source -eq 'WeeklyJson') {
         Write-Progress -Activity "Building service tag cache" -Status "Downloading weekly JSON file" -PercentComplete 0
+        Write-Verbose "$($MyInvocation.MyCommand): Fetching cache from weekly json"
 
         $JsonData = Get-ServiceTagJson
         $ServiceTags = (ConvertFrom-Json -InputObject $JsonData).values.properties | Where-Object { -not [String]::IsNullOrEmpty($_.region) }
     } Else {
         Write-Progress -Activity "Building service tag cache" -Status "Fetching service tags from API" -PercentComplete 0
+        Write-Verbose "$($MyInvocation.MyCommand): Fetching cache using service tag API"
+
         $ServiceTags = (Get-AzNetworkServiceTag -Location 'eastus').Values.Properties | Where-Object { -not [String]::IsNullOrEmpty($_.Region) }
     }
 
     $i = 0
-    $Cache = Foreach ($ServiceTag in $ServiceTags) {
+    $CalculatedRanges = Foreach ($ServiceTag in $ServiceTags) {
         $Progress = $i * 80 / $ServiceTags.Count
         $i += 1
         Write-Progress -Activity "Building service tag cache. This can take a moment." -Status "$([math]::Round($Progress, 0))% Calculating $($ServiceTag.Region) ranges" -PercentComplete $Progress
@@ -41,11 +44,16 @@ Function New-AzServiceTagCache {
         }
     }
 
+    # Add timestamp to cache
+    $Cache = [pscustomobject]@{
+        Date = Get-Date
+        Cache = $CalculatedRanges
+    }
+
     # Save cache to location
     If($Path) {
-        [pscustomobject]@{
-
-        } | ConvertTo-Json -Depth 10 | Out-File $Path -Encoding utf8
+        Write-Verbose "$($MyInvocation.MyCommand): Saving cache to $Path"
+        $Cache | ConvertTo-Json -Depth 10 | Out-File $Path -Encoding utf8
     }
 
     # Make cache usable in module
--- PSAzureLocator/Public/Get-AzResourceLocation.ps1
+++ PSAzureLocator/Public/Get-AzResourceLocation.ps1
@@ -1,39 +1,63 @@
 Function Get-AzResourceLocation {
     [CmdletBinding()]
     Param(
-        [String]$DnsName
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
+        [Alias('DnsName', 'IpAddress', 'Host')]
+        [String]$Target
     )
 
-    $IpAddresses = [System.Net.Dns]::GetHostAddresses($DnsName).IPAddressToString
-
-    $Cache = Get-Variable -Scope Script -Name ServiceTagCache -ValueOnly -ErrorAction SilentlyContinue
-
-    If (-not $Cache) {
-        Write-Verbose "$($MyInvocation.MyCommand): No cache has been loaded yet. Searching for saved cache."
-
-        $Cache = Get-AzServiceTagCache -Path $constant_CacheFile
-        Set-Variable -Scope Script -Name ServiceTagCache -Value $Cache
-
+    Begin {
+        # $Cache = Get-Variable -Scope Script -Name ServiceTagCache -ValueOnly -ErrorAction SilentlyContinue
+    
         If (-not $Cache) {
-            Write-Verbose "$($MyInvocation.MyCommand): No saved cache has been found. Creating it now."
-            $Cache = New-AzServiceTagCache -Path $constant_CacheFile
+            Write-Verbose "$($MyInvocation.MyCommand): No cache has been loaded yet. Searching for saved cache."
+    
+            $Cache = Get-AzServiceTagCache -Path $constant_CacheFile
+            Set-Variable -Scope Script -Name ServiceTagCache -Value $Cache
+    
+            If (-not $Cache) {
+                Write-Verbose "$($MyInvocation.MyCommand): No saved cache has been found. Creating it now."
+                $Cache = New-AzServiceTagCache -Path $constant_CacheFile
+            } Else {
+                Write-Verbose "$($MyInvocation.MyCommand): Saved cache has been found"
+            }
+
+
         } Else {
-            Write-Verbose "$($MyInvocation.MyCommand): Saved cache has been found and loaded."
+            Write-Verbose "$($MyInvocation.MyCommand): Session cache will be used."
         }
-    } Else {
-        Write-Verbose "$($MyInvocation.MyCommand): Session cache will be used."
     }
 
-    Foreach($IPAddress in $IpAddresses) {
-        Foreach ($ServiceTag in $Cache) {
-            Foreach ($Subnet in $ServiceTag.Subnets) {
-                If(Test-IpInSubnet -IpAddress $IpAddress -Subnet $Subnet.Subnet -SubnetMask $Subnet.SubnetMask) {
-                    [pscustomobject]@{
-                        IpAddress     = $IpAddress
-                        Region        = $ServiceTag.Name
-                        SystemService = $ServiceTag.SystemService
-                        ChangeNumber  = $ServiceTag.ChangeNumber
-                        CidrBlock     = $Subnet.Subnet + '/' + $Subnet.Cidr
+    Process {
+        # Check what kind of target we are dealing with
+        # Simple regex test
+        # [System.Net.IPAddress]::TryParse() might be a better option
+        Write-Verbose "$($MyInvocation.MyCommand): Target: $Target"
+        If($Target -notmatch '^(\d+\.){3}\d+') {
+            # Get host addresses
+            $IpAddresses = [System.Net.Dns]::GetHostAddresses($Target).IPAddressToString
+        } Else {
+            # Take target as is
+            $IpAddresses = $Target
+        }
+        
+        # We are treating the target address as collection multiple dns records could have been returned
+        Foreach($IpAddress in $IpAddresses) {
+            Write-Verbose "$($MyInvocation.MyCommand): Find subnet for address: $IpAddress"
+
+            Foreach ($ServiceTag in $Cache.Cache) {
+                Foreach ($Subnet in $ServiceTag.Subnets) {
+                    If(Test-IpInSubnet -IpAddress $IpAddress -Subnet $Subnet.Subnet -SubnetMask $Subnet.SubnetMask) {
+                        [pscustomobject]@{
+                            IpAddress     = $IpAddress
+                            Region        = $ServiceTag.Name
+                            SystemService = $ServiceTag.SystemService
+                            ChangeNumber  = $ServiceTag.ChangeNumber
+                            CidrBlock     = $Subnet.Subnet + '/' + $Subnet.Cidr
+                        }
                     }
                 }
             }
